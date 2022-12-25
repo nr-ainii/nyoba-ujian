f8a97f009c7198097faccaf70a7e88d0924f6703
nr-ainii <aininr.azizah@gmail.com>
2022-12-25 22:04:02 +0700

    add comment

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
-const app = express();
+const app = express(); 
+// add middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //
 app.use(UserRoute);
 
-app.listen(5000, () => console.log("Server running..."));
+app.listen(5000, ()=> console.log("Server running...")); //handling json port
